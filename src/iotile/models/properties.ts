1ffd0c0f4233d3f4065b1d5b85f26906b7396704
Vanielle Le <vanielle@arch-iot.com>
2017-06-12 18:00:48 -0700

    updating properties field on device

--- src/iotile/models/properties.ts
+++ src/iotile/models/properties.ts
@@ -1,35 +1,8 @@
 export class Properties {
-  public shipFrom: string;
-  public shipTo: string;
-  public shipVia: string;
-  public loadingType: string;
-  public transportType: string;
-  public cargoDescription: string;
 
-  constructor(data) {
-    if (data.shipFrom) {
-      this.shipFrom = data.shipFrom;
-    }
-
-    if (data.shipTo) {
-      this.shipTo = data.shipTo;
-    }
-
-    if (data.shipVia) {
-      this.shipVia = data.shipVia;
-    }
-
-    if (data.loadingType) {
-      this.loadingType = data.loadingType;
-    }
-
-    if (data.transportType) {
-      this.transportType = data.transportType;
-    }
-
-    if (data.cargoDescription) {
-      this.cargoDescription = data.cargoDescription;
+  constructor(datas) {
+    for (let data of datas) {
+      this[data.name] = data.value;
     }
   }
-
 }
