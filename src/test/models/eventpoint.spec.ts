99aa93b959771b4b87c94f2682e89c9d73739950
david Karchmer <dkarchmer@gmail.com>
2017-06-21 23:59:17 -0700

    Add EventPoint.summaryData

--- src/test/models/eventpoint.spec.ts
+++ src/test/models/eventpoint.spec.ts
@@ -17,12 +17,30 @@ describe('EventPointTest', () => {
       "device_timestamp": null,
       "timestamp": "2016-03-25T05:13:38Z",
       "streamer_local_id": 1,
-      "dirty_ts": false
+      "dirty_ts": false,
+      "summary": {
+        "pressure": 4.7,
+        "max_g": 0.48828125,
+        "min_y": -0.5218505859375,
+        "max_z": 0.15869140625,
+        "min_z": -0.1953125,
+        "max_y": 0.48828125,
+        "temperature": 21.1875,
+        "max_x": 0.115966796875,
+        "min_g": -0.5218505859375,
+        "min_x": -0.079345703125,
+        "relative_humidity": 35.5859375,
+        "foo": "bar"
+      }
     });
     expect(point.stream).toEqual("s--0000-0007--0000-0000-0000-0001--6000");
     expect(point.timestamp.getFullYear()).toEqual(2016);
     expect(point.incrementalId).toEqual(1);
     expect(point.dirtyTimestamp).toBeFalsy();
+    expect(point.summaryData['pressure']).toEqual(4.7);
+    expect(point.summaryData['temperature']).toEqual(21.1875);
+    expect(point.summaryData['foo']).toEqual('bar');
+    expect(point.summaryData['foobar']).toBeUndefined();
   });
 
 });
