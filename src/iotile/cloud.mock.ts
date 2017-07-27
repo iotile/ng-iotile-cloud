c17b51e35da39abb331f2232bcfaecfbf1b15196
Vanielle Le <vanielle@arch-iot.com>
2017-07-27 10:13:09 -0700

    fix test"

--- src/iotile/cloud.mock.ts
+++ src/iotile/cloud.mock.ts
@@ -493,37 +493,37 @@ export class CloudServiceMock {
     let devices: Array<Device> = [];
 
     let device1 = new Device({
-      "id": 130,
-      "slug": "d--0000-0000-0000-0082",
-      "gid": "0000-0000-0000-0082",
-      "label": "South Field",
-      "active": true,
-      "firmware_versions": [],
-      "sg": "water-meter-v1-1-0",
-      "template": "1d1p2bt103-v3-0-0",
-      "org": "arch-farms",
-      "project": "106d8ab9-3e5c-4834-8d90-d130a020058d",
-      "lat": null,
-      "lon": null,
-      "created_on": "2017-03-25T00:47:43.690784Z"
+      'id': 130,
+      'slug': 'd--0000-0000-0000-0082',
+      'gid': '0000-0000-0000-0082',
+      'label': 'South Field',
+      'active': true,
+      'firmware_versions': [],
+      'sg': 'water-meter-v1-1-0',
+      'template': '1d1p2bt103-v3-0-0',
+      'org': 'arch-farms',
+      'project': '106d8ab9-3e5c-4834-8d90-d130a020058d',
+      'lat': null,
+      'lon': null,
+      'created_on': '2017-03-25T00:47:43.690784Z'
     });
 
     devices.push(device1);
 
     let device2 = new Device({
-      "id": 173,
-      "slug": "d--0000-0000-0000-00ad",
-      "gid": "0000-0000-0000-00ad",
-      "label": "IOTile Device (00ad)",
-      "active": true,
-      "firmware_versions": [],
-      "sg": "single-soil-moisture-v2-0-0",
-      "template": "rpigateway-v0-1-0",
-      "org": "arch-farms",
-      "project": "106d8ab9-3e5c-4834-8d90-d130a020058d",
-      "lat": null,
-      "lon": null,
-      "created_on": "2017-03-31T01:14:52.254065Z"
+      'id': 173,
+      'slug': 'd--0000-0000-0000-00ad',
+      'gid': '0000-0000-0000-00ad',
+      'label': 'IOTile Device (00ad)',
+      'active': true,
+      'firmware_versions': [],
+      'sg': 'single-soil-moisture-v2-0-0',
+      'template': 'rpigateway-v0-1-0',
+      'org': 'arch-farms',
+      'project': '106d8ab9-3e5c-4834-8d90-d130a020058d',
+      'lat': null,
+      'lon': null,
+      'created_on': '2017-03-31T01:14:52.254065Z'
     });
 
     devices.push(device2);
@@ -533,103 +533,103 @@ export class CloudServiceMock {
     let variables: Array<Variable> = [];
 
     let variable1 = new Variable({
-      "id": "7abc2be6-cc7e-4f37-8fba-cfe758f7dd0a",
-      "name": "Odometer 1",
-      "lid": 4109,
-      "var_type": "water-meter-volume",
-      "input_unit": {
-        "slug": "in--water-meter-volume--gallons",
-        "unit_full": "Gallons",
-        "unit_short": "G",
-        "m": 378541,
-        "d": 100000,
-        "o": 0.0
+      'id': '7abc2be6-cc7e-4f37-8fba-cfe758f7dd0a',
+      'name': 'Odometer 1',
+      'lid': 4109,
+      'var_type': 'water-meter-volume',
+      'input_unit': {
+        'slug': 'in--water-meter-volume--gallons',
+        'unit_full': 'Gallons',
+        'unit_short': 'G',
+        'm': 378541,
+        'd': 100000,
+        'o': 0.0
       },
-      "output_unit": {
-        "slug": "out--water-meter-volume--gallons",
-        "unit_full": "Gallons",
-        "unit_short": "G",
-        "m": 100000,
-        "d": 378541,
-        "o": 0.0,
-        "decimal_places": 0,
-        "derived_units": {
-          "rate": {
-            "gph": {
-              "m": 6,
-              "d": 1
+      'output_unit': {
+        'slug': 'out--water-meter-volume--gallons',
+        'unit_full': 'Gallons',
+        'unit_short': 'G',
+        'm': 100000,
+        'd': 378541,
+        'o': 0.0,
+        'decimal_places': 0,
+        'derived_units': {
+          'rate': {
+            'gph': {
+              'm': 6,
+              'd': 1
             },
-            "gpm": {
-              "m": 1,
-              "d": 10
+            'gpm': {
+              'm': 1,
+              'd': 10
             }
           }
         }
       },
-      "derived_variable": null,
-      "project": "106d8ab9-3e5c-4834-8d90-d130a020058d",
-      "org": "arch-farms",
-     "about": "",
-     "created_on": "2017-03-25T00:47:43.979302Z",
-     "type": "N/A",
-     "raw_value_format": "<L",
-     "units": "",
-     "multiplication_factor": 1,
-     "division_factor": 1,
-     "offset": 0.0,
-     "decimal_places": 2,
-     "mdo_label": "",
-     "web_only": false,
-     "app_only": true,
-     "slug": "v--0000-0009--100d"
+      'derived_variable': null,
+      'project': '106d8ab9-3e5c-4834-8d90-d130a020058d',
+      'org': 'arch-farms',
+     'about': '',
+     'created_on': '2017-03-25T00:47:43.979302Z',
+     'type': 'N/A',
+     'raw_value_format': '<L',
+     'units': '',
+     'multiplication_factor': 1,
+     'division_factor': 1,
+     'offset': 0.0,
+     'decimal_places': 2,
+     'mdo_label': '',
+     'web_only': false,
+     'app_only': true,
+     'slug': 'v--0000-0009--100d'
     });
 
-    variables.push(variable1)
+    variables.push(variable1);
 
     let variable2 = new Variable({
-      "id": "04b35860-fe62-4897-a2c9-500818fbd4bd",
-      "name": "Pulse 1",
-      "lid": 4107,
-      "var_type": "water-meter-flow",
-      "input_unit": {
-        "slug": "in--water-meter-flow--gallons",
-        "unit_full": "Gallons",
-        "unit_short": "G",
-        "m": 3785,
-        "d": 65536000,
-        "o": 0.0
+      'id': '04b35860-fe62-4897-a2c9-500818fbd4bd',
+      'name': 'Pulse 1',
+      'lid': 4107,
+      'var_type': 'water-meter-flow',
+      'input_unit': {
+        'slug': 'in--water-meter-flow--gallons',
+        'unit_full': 'Gallons',
+        'unit_short': 'G',
+        'm': 3785,
+        'd': 65536000,
+        'o': 0.0
       },
-      "output_unit": {
-        "slug": "out--water-meter-flow--gallons-per-min",
-        "unit_full": "Gallons per Min",
-        "unit_short": "GPM",
-        "m": 100000,
-        "d": 378541,
-        "o": 0.0,
-        "decimal_places": 1,
-        "derived_units": {}
+      'output_unit': {
+        'slug': 'out--water-meter-flow--gallons-per-min',
+        'unit_full': 'Gallons per Min',
+        'unit_short': 'GPM',
+        'm': 100000,
+        'd': 378541,
+        'o': 0.0,
+        'decimal_places': 1,
+        'derived_units': {}
       },
-      "derived_variable": null,
-      "project": "106d8ab9-3e5c-4834-8d90-d130a020058d",
-      "org": "arch-farms",
-      "about": "",
-      "created_on": "2017-03-25T00:47:43.868087Z",
-      "type": "N/A",
-      "raw_value_format": "<L",
-      "units": "",
-      "multiplication_factor": 1,
-      "division_factor": 1,
-      "offset": 0.0,
-      "decimal_places": 2,
-      "mdo_label": "",
-      "web_only": false,
-      "app_only": true,
-      "slug": "v--0000-0009--100b"
+      'derived_variable': null,
+      'project': '106d8ab9-3e5c-4834-8d90-d130a020058d',
+      'org': 'arch-farms',
+      'about': '',
+      'created_on': '2017-03-25T00:47:43.868087Z',
+      'type': 'N/A',
+      'raw_value_format': '<L',
+      'units': '',
+      'multiplication_factor': 1,
+      'division_factor': 1,
+      'offset': 0.0,
+      'decimal_places': 2,
+      'mdo_label': '',
+      'web_only': false,
+      'app_only': true,
+      'slug': 'v--0000-0009--100b'
     });
 
-    variables.push(variable2)
+    variables.push(variable2);
 
-    proj.addVariables(variables)
+    proj.addVariables(variables);
 
     returnedData.next(proj);
 
@@ -642,89 +642,89 @@ export class CloudServiceMock {
     let returnedData = new ReplaySubject(1);
 
     let device = new Device({
-      "id": 173,
-      "slug": "d--0000-0000-0000-00ad",
-      "gid": "0000-0000-0000-00ad",
-      "label": "IOTile Device (00ad)",
-      "active": true,
-      "firmware_versions": [],
-      "sg": "single-soil-moisture-v2-0-0",
-      "template": "rpigateway-v0-1-0",
-      "org": "arch-farms",
-      "project": "106d8ab9-3e5c-4834-8d90-d130a020058d",
-      "lat": null,
-      "lon": null,
-      "created_on": "2017-03-31T01:14:52.254065Z"
+      'id': 173,
+      'slug': 'd--0000-0000-0000-00ad',
+      'gid': '0000-0000-0000-00ad',
+      'label': 'IOTile Device (00ad)',
+      'active': true,
+      'firmware_versions': [],
+      'sg': 'single-soil-moisture-v2-0-0',
+      'template': 'rpigateway-v0-1-0',
+      'org': 'arch-farms',
+      'project': '106d8ab9-3e5c-4834-8d90-d130a020058d',
+      'lat': null,
+      'lon': null,
+      'created_on': '2017-03-31T01:14:52.254065Z'
     });
 
     let sensorgraph = new SensorGraph({
-      "id": 3,
-      "name": "Single Soil Moisture",
-      "slug": "single-soil-moisture-v2-0-0",
-      "org": "arch-systems",
-      "project_template": "default-template-v1-0-0",
-      "variable_templates": [
+      'id': 3,
+      'name': 'Single Soil Moisture',
+      'slug': 'single-soil-moisture-v2-0-0',
+      'org': 'arch-systems',
+      'project_template': 'default-template-v1-0-0',
+      'variable_templates': [
         {
-          "id": 7,
-          "label": "IO 1",
-          "lid_hex": "5003",
-          "derived_lid_hex": "",
-          "var_type": "soil-moisture-percent",
-          "default_input_unit": "in--soil-moisture-percent--percent",
-          "default_output_unit": "out--soil-moisture-percent--percent",
-          "ctype": "unsigned int",
-          "m": 1,
-          "d": 1,
-          "o": 0.0,
-          "app_only": false,
-          "web_only": false
+          'id': 7,
+          'label': 'IO 1',
+          'lid_hex': '5003',
+          'derived_lid_hex': '',
+          'var_type': 'soil-moisture-percent',
+          'default_input_unit': 'in--soil-moisture-percent--percent',
+          'default_output_unit': 'out--soil-moisture-percent--percent',
+          'ctype': 'unsigned int',
+          'm': 1,
+          'd': 1,
+          'o': 0.0,
+          'app_only': false,
+          'web_only': false
         },
         {
-          "id": 8,
-          "label": "Sensor 1",
-          "lid_hex": "100f",
-          "derived_lid_hex": "",
-          "var_type": "soil-moisture-percent",
-          "default_input_unit": "in--soil-moisture-percent--percent",
-          "default_output_unit": "out--soil-moisture-percent--percent",
-          "ctype": "unsigned int",
-          "m": 1,
-          "d": 1,
-          "o": 0.0,
-          "app_only": true,
-          "web_only": false
+          'id': 8,
+          'label': 'Sensor 1',
+          'lid_hex': '100f',
+          'derived_lid_hex': '',
+          'var_type': 'soil-moisture-percent',
+          'default_input_unit': 'in--soil-moisture-percent--percent',
+          'default_output_unit': 'out--soil-moisture-percent--percent',
+          'ctype': 'unsigned int',
+          'm': 1,
+          'd': 1,
+          'o': 0.0,
+          'app_only': true,
+          'web_only': false
         }
       ],
-      "display_widget_templates": [
+      'display_widget_templates': [
         {
-          "id": 7,
-          "label": "IO 1",
-          "lid_hex": "5003",
-          "var_type": "soil-moisture-percent",
-          "derived_unit_type": "",
-          "show_in_app": false,
-          "show_in_web": true
+          'id': 7,
+          'label': 'IO 1',
+          'lid_hex': '5003',
+          'var_type': 'soil-moisture-percent',
+          'derived_unit_type': '',
+          'show_in_app': false,
+          'show_in_web': true
         },
         {
-          "id": 8,
-          "label": "IO Realtime 1",
-          "lid_hex": "100f",
-          "var_type": "soil-moisture-percent",
-          "derived_unit_type": "",
-          "show_in_app": true,
-          "show_in_web": false
+          'id': 8,
+          'label': 'IO Realtime 1',
+          'lid_hex': '100f',
+          'var_type': 'soil-moisture-percent',
+          'derived_unit_type': '',
+          'show_in_app': true,
+          'show_in_web': false
         }
       ],
-      "ui_extra": {
-        "web": {
-          "pageTemplateSlug": "default"
+      'ui_extra': {
+        'web': {
+          'pageTemplateSlug': 'default'
         },
-        "mobile": { }
+        'mobile': { }
       },
-      "major_version": 2,
-      "minor_version": 0,
-      "patch_version": 0,
-      "created_on": "2017-03-25T00:46:03.712704Z"
+      'major_version': 2,
+      'minor_version': 0,
+      'patch_version': 0,
+      'created_on': '2017-03-25T00:46:03.712704Z'
     });
 
     device.sg = sensorgraph;
@@ -736,31 +736,31 @@ export class CloudServiceMock {
   }
 
   public getProject(projectId: string): Observable<Project> {
-    let mockProjectId = "2c8dadd7-add0-4157-90cd-036bcc178ec9";
+    let mockProjectId = '2c8dadd7-add0-4157-90cd-036bcc178ec9';
     projectId = mockProjectId;
 
     let project = new Project({
-      "id": "106d8ab9-3e5c-4834-8d90-d130a020058d",
-      "name": "Water Usage",
-      "slug": "p--0000-0009",
-      "gid": "0000-0009",
-      "org": "arch-farms",
-      "about": "South Fields",
-      "project_template": "water-meter-permanent-v1-0-0",
-      "page": {
-        "slug": "water-meter",
-        "id": 2,
-        "label": "Water Meter"
+      'id': '106d8ab9-3e5c-4834-8d90-d130a020058d',
+      'name': 'Water Usage',
+      'slug': 'p--0000-0009',
+      'gid': '0000-0009',
+      'org': 'arch-farms',
+      'about': 'South Fields',
+      'project_template': 'water-meter-permanent-v1-0-0',
+      'page': {
+        'slug': 'water-meter',
+        'id': 2,
+        'label': 'Water Meter'
       },
-      "created_on": "2017-03-25T00:47:43.630348Z",
-      "created_by": "david"
+      'created_on': '2017-03-25T00:47:43.630348Z',
+      'created_by': 'david'
     });
 
     return Observable.of(project);
   }
 
   fetchProjectWithAssociatedData(projectId): Observable<Project> {
-    let mockProjectId = "2c8dadd7-add0-4157-90cd-036bcc178ec9";
+    let mockProjectId = '2c8dadd7-add0-4157-90cd-036bcc178ec9';
     projectId = mockProjectId;
 
     return this.getProject(projectId).flatMap((p: Project) => {
