37928c6b2df133c69a5cab3cc4721d2954702249
Vanielle Le <lekosfmi@users.noreply.github.com>
2017-08-28 23:09:25 -0500

    Feat archive for org (#51)
    * adding jasmine-core to test
    
    * archive model
    
    * adding getArchiveForOrg method
    
    * fixing test
    
    * bump version
    Resolved #50

---
+++ src/iotile/models/archive.ts
@@ -0,0 +1,19 @@
+export class Archive {
+  public id: number;
+  public slug: string;
+  public title: string;
+  public org: string;
+  public block: number;
+  public createdOn: Date;
+  public createdBy: Date;
+
+  public constructor(data) {
+    this.id = data.id;
+    this.slug = data.slug;
+    this.title = data.title;
+    this.org = data.org;
+    this.block = data.block;
+    this.createdOn = new Date(data.created_on);
+    this.createdBy = new Date(data.created_by);
+  }
+}
