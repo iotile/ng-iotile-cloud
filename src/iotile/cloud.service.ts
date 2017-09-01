d274fdd166712abc658a9c076857f28c84033051
Vanielle Le <vanielle@arch-iot.com>
2017-09-01 15:07:49 -0700

    fix spacing

--- src/iotile/cloud.service.ts
+++ src/iotile/cloud.service.ts
@@ -729,12 +729,12 @@ export class CloudService {
   }
 
   public getDataBlock(dataBlockSlug: string): Observable<DataBlock> {
-   let url = '/datablock/' + dataBlockSlug + '/';
-
-   return this._get(url).map((data: any) => {
-     return new DataBlock(data);
-   }, err => console.error(err));
- }
+    let url = '/datablock/' + dataBlockSlug + '/';
+    
+    return this._get(url).map((data: any) => {
+      return new DataBlock(data);
+    }, err => console.error(err));
+  }
 
   public postDataBlock(
     payload: {
