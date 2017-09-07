8f09ba6e0fe95c9b5815376a8f75dc3e48146cda
Vanielle Le <vanielle@arch-iot.com>
2017-09-06 19:04:34 -0700

    getPostPayload for property model

--- src/iotile/models/property.ts
+++ src/iotile/models/property.ts
@@ -6,10 +6,23 @@ export class Property {
   public id: number;
   public name: string;
   public value: string;
+  public type: string;
 
   constructor(data) {
     this.id = data.id;
     this.name = data.name;
     this.value = data.value;
+    this.type = data.type;
+  }
+
+  getPostPayload() {
+    let payload = {};
+    let type = this.type + '_value';
+
+    payload['id'] = this.id;
+    payload['name'] = this.name;
+    payload[type] = this.value;
+
+    return payload;
   }
 }
