c27111b8604c1f518dad53941e442cca4d1956b7
Vanielle Le <vanielle@arch-iot.com>
2017-09-06 19:04:55 -0700

    testing model for basic and different types

--- src/test/models/property.spec.ts
+++ src/test/models/property.spec.ts
@@ -6,7 +6,22 @@ describe('PropertyTest', () => {
   const dummyProperty1 = new Property({
     id: 8,
     name: 'cargoDescription',
-    value: 'Statement or description of the cargo.'
+    value: 'Statement or description of the cargo.',
+    type: 'str'
+  });
+
+  const dummyProperty2 = new Property({
+    id: 8,
+    name: 'distance',
+    value: 100,
+    type: 'int'
+  });
+
+  const dummyProperty3 = new Property({
+    id: 8,
+    name: 'By Plane',
+    value: true,
+    type: 'bool'
   });
 
   it('check basic property', () => {
@@ -15,4 +30,27 @@ describe('PropertyTest', () => {
     expect(dummyProperty1.name).toEqual('cargoDescription');
   });
 
+  describe('getPostPayload()', () => {
+
+    it('checks basic', () => {
+      let dummyPayload = dummyProperty1.getPostPayload();
+      expect(dummyPayload['id']).toEqual(8);
+      expect(dummyPayload['name']).toBe('cargoDescription');
+    });
+
+    it('checks str_value', () => {
+      let dummyPayload = dummyProperty1.getPostPayload();
+      expect(dummyPayload['str_value']).toBe('Statement or description of the cargo.');
+    });
+
+    it('checks int_value', () => {
+      let dummyPayload = dummyProperty2.getPostPayload();
+      expect(dummyPayload['int_value']).toBe(100);
+    });
+
+    it('checks bool_value', () => {
+      let dummyPayload = dummyProperty3.getPostPayload();
+      expect(dummyPayload['bool_value']).toBe(true);
+    });
+  });
 });
