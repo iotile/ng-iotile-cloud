f7ca60ffbcb6eadc6131967e347accbc4679a533
Vanielle Le <vanielle@arch-iot.com>
2018-01-30 20:00:03 -0800

    test

--- tests/models/member.spec.ts
+++ tests/models/member.spec.ts
@@ -27,4 +27,29 @@ describe('MemberTest', () => {
 
     expect(member.isOrgAdmin).toBe(false);
   });
+
+  it('checks model user details', () => {
+    let member: Member = new Member({
+      "user": "lekosfmi",
+      "created_on": "2017-04-13T03:12:39Z",
+      "is_active": true,
+      "is_org_admin": true,
+      "user_details": {
+        "email": "vanielle@arch-iot.com",
+        "username": "lekosfmi",
+        "name": "Vanielle Lee",
+        "tagline": "",
+        "avatar": {
+          "thumbnail": "https://secure.gravatar.com/avatar/0828c899dcc84c7b50ac44b5a154b10c?d=identicon&s=80",
+          "tiny": "https://secure.gravatar.com/avatar/0828c899dcc84c7b50ac44b5a154b10c?d=identicon&s=28"
+        }
+      }
+    });
+
+    expect(member.userDetails.email).toBe('vanielle@arch-iot.com');
+    expect(member.userDetails.username).toBe('lekosfmi');
+    expect(member.userDetails.name).toBe('Vanielle Lee');
+    expect(member.userDetails.tagline).toBe('');
+    expect(member.userDetails.avatarUrl).toBe('https://secure.gravatar.com/avatar/0828c899dcc84c7b50ac44b5a154b10c?d=identicon&s=80');
+  });
 });
