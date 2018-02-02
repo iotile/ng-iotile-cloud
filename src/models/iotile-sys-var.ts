f8e66b0403b9d99a085a9722285c4ebc3dcd468c
Vanielle Le <vanielle@arch-iot.com>
2018-02-01 20:11:23 -0800

    iotile-sys-var model and test

---
+++ src/models/iotile-sys-var.ts
@@ -0,0 +1,6 @@
+export class IOTileSysVar {
+  public gatewayScanBegin: string = '5a00';
+  public gatewayScanEnd: string = '5a01';
+  public gatewayScanFailure: string = '5a03';
+  public endOfTripSummary: string = '5a07';
+}
