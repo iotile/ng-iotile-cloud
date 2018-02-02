608838ba44dfd6c823be9a83b65af39e70ab748e
Vanielle Le <vanielle@arch-iot.com>
2018-02-02 09:33:23 -0800

    remove user var and rename reboot

--- src/models/iotile-sys-var.ts
+++ src/models/iotile-sys-var.ts
@@ -1,9 +1,4 @@
 export class IOTileSysVar {
-  public event: string = '5020';
-  public pressure: string = '5021';
-  public humidity: string = '5022';
-  public temperature: string = '5023';
-
   public battery: string = '5800';
 
   public gatewayScanBegin: string = '5a00';
@@ -11,7 +6,7 @@ export class IOTileSysVar {
   public gatewayScanFailure: string = '5a03';
   public endOfTripSummary: string = '5a07';
 
-  public rebootStream: string = '5c00';
+  public reboot: string = '5c00';
 
   public tripStarted: string = '5E00';
   public tripEnded: string = '5E01';
