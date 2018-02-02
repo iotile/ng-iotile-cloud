e61df9c2586e316c5bbc489ff9eccb9d6a50b30b
Vanielle Le <vanielle@arch-iot.com>
2018-02-01 20:51:24 -0800

    trip started/ended, stream event

--- src/models/iotile-sys-var.ts
+++ src/models/iotile-sys-var.ts
@@ -1,4 +1,8 @@
 export class IOTileSysVar {
+  public eventStream: string = '5020';
+
+  public battery: string = '5800';
+
   public gatewayScanBegin: string = '5a00';
   public gatewayScanEnd: string = '5a01';
   public gatewayScanFailure: string = '5a03';
@@ -6,5 +10,7 @@ export class IOTileSysVar {
 
   public rebootStream: string = '5c00';
 
-  public battery: string = '5800';
+  public tripStarted: string = '5E00';
+  public tripEnded: string = '5E01';
+  public tripPauseResumse: string = '5E02';
 }
