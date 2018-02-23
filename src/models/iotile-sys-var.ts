24c931b6545fd80b6f8402852b9df58d0d4b288d
Vanielle Lee <vanielle@lekosfmi.com>
2018-02-23 15:45:56 -0800

    Feat fix typo (#95)
    * 5e00, 5e01
    
    * typo
    
    * patch update
    
    * update package-lock
    
    * middle of trip summary

--- src/models/iotile-sys-var.ts
+++ src/models/iotile-sys-var.ts
@@ -5,10 +5,11 @@ export class IOTileSysVar {
   public gatewayScanEnd: string = '5a01';
   public gatewayScanFailure: string = '5a03';
   public endOfTripSummary: string = '5a07';
+  public middleOfTripSummary: string = '5a08';
 
   public reboot: string = '5c00';
 
-  public tripStarted: string = '5E00';
-  public tripEnded: string = '5E01';
-  public tripRecording: string = '5E02';
+  public tripStarted: string = '5e00';
+  public tripEnded: string = '5e01';
+  public tripRecording: string = '5e02';
 }
