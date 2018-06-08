e6a21d913dd033afb202c41674d8bcda589138e7
fil-forti <inf.fforti@gmail.com>
2018-06-08 14:17:57 -0700

    Update api filter (#125)
    Add method to remove filter
    Add parameter to add unique filter

--- src/models/apifilter.ts
+++ src/models/apifilter.ts
@@ -11,8 +11,18 @@ export class ApiFilter {
       return '';
     }
 
-    public addFilter(name: string, value: string): void {
+    public addFilter(name: string, value: string, unique: boolean = false): void {
       let arg: string = name + '=' + value;
+      if (unique) {
+        this.removeFilter(name);
+      }
       this.filters.push(arg);
+    }
+
+    public removeFilter(name: string): void {
+      let arg: string = name + '=';
+      this.filters = this.filters.filter(item =>
+        item.indexOf(arg) !== 0
+      );
     }
   }
