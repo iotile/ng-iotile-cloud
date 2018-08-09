a0961d56d0f244d621b92c92b7a73ac5c4e71bd6
Vanielle Lee <vanielle@lekosfmi.com>
2018-08-09 16:59:18 -0700

    Add 5a09 (#134)

--- src/models/iotile-sys-var.ts
+++ src/models/iotile-sys-var.ts
@@ -1,3 +1,8 @@
+/**
+ * Refer to the link for list of SysVar available on Classic:
+ * https://github.com/iotile/strato/blob/master/server/apps/utils/iotile/variable.py
+ */
+
 export class IOTileSysVar {
   public battery: string = '5800';
 
@@ -6,6 +11,7 @@ export class IOTileSysVar {
   public gatewayScanFailure: string = '5a03';
   public endOfTripSummary: string = '5a07';
   public middleOfTripSummary: string = '5a08';
+  public deviceDataMask: string = '5a09';
 
   public reboot: string = '5c00';
 
